6ca686d5f7c8fb33f17097f02554bd339b6f6ce6
Udit <uditsaurabh747@gmail.com>
2025-05-24 13:23:26 +0530

    add allerror code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,60 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { useAuth } from '@clerk/clerk-react';
+import { useUser } from '@clerk/clerk-react';
+import { useState } from 'react';
 
-export default function App() {
+const App = () => {
+    const { getToken, isSignedIn } = useAuth();
+    const[error,setError]=useState()
+    const[success,setSuccess]=useState()
+    const { user } =  useUser();
+    console.log(isSignedIn)
+    console.log(user)
+
+
+    const fetchData = async () => {
+        const query = `
+    {
+        page(id:"61576807405147") {
+        name
+        fan_count
+    }
+    }
+`;
+    const accessToken = await getToken(); // Gets JWT token
+    console.log(accessToken)
+    fetch('https://graph.facebook.com/graphql', {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${accessToken}`,
+  },
+  body: JSON.stringify({ query }),
+})
+  .then(res => res.json())
+  .then(data => {
+    setSuccess(data)
+    console.log(data)})
+  .catch(err => {
+    setError(error)
+    console.error('Error:', err)});
+  };
     return (
         <div>
-            <h1>Social media signup example</h1>
-        <header>
-            <SignedOut>
-                <SignInButton/>
-            </SignedOut>
-            <SignedIn>
-                <UserButton/>
-            </SignedIn>
-        </header>
+            <h1 className="text-3xl font-bold underline">Social media signup example</h1>
+            <button onClick={fetchData}>Get Token</button>
+            <header className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-2">
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </header>
+            {JSON.stringify(success)}
+            {JSON.stringify(error)}
         </div>
     );
 }
+
+export default App
