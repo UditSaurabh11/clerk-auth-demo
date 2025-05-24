7b69e87e9812b3376e78b1ea263c5cb7ba729af6
Udit <uditsaurabh747@gmail.com>
2025-05-24 13:46:09 +0530

    add all changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-reac
 import { useAuth } from '@clerk/clerk-react';
 import { useUser } from '@clerk/clerk-react';
 import { useState } from 'react';
+import axios from "axios"
 
 const App = () => {
     const { getToken, isSignedIn } = useAuth();
-    const[error,setError]=useState()
-    const[success,setSuccess]=useState()
-    const { user } =  useUser();
+    const [error, setError] = useState()
+    const [success, setSuccess] = useState()
+    const { user } = useUser();
     console.log(isSignedIn)
     console.log(user)
 
@@ -20,25 +21,56 @@ const App = () => {
         fan_count
     }
     }
+    
 `;
-    const accessToken = await getToken(); // Gets JWT token
-    console.log(accessToken)
-    fetch('https://graph.facebook.com/graphql', {
-    method: 'POST',
-    headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${accessToken}`,
-  },
-  body: JSON.stringify({ query }),
-})
-  .then(res => res.json())
-  .then(data => {
-    setSuccess(data)
-    console.log(data)})
-  .catch(err => {
-    setError(error)
-    console.error('Error:', err)});
-  };
+        const accessToken = await getToken(); // Gets JWT token
+        console.log(accessToken)
+
+        try {
+
+            const pagesRes = await axios.get(`https://graph.facebook.com/v19.0/me/accounts`, {
+                params: { access_token: accessToken },
+            });
+            console.log("pageres --->",pagesRes)
+            const page = pagesRes.data.data[0];
+            const pageToken = page.access_token;
+
+            const formsRes = await axios.get(`https://graph.facebook.com/v19.0/${page.id}/leadgen_forms`, {
+                params: { access_token: pageToken },
+            });
+            console.log("formres --->",pagesRes)    
+            const formId = formsRes.data.data[0].id;
+                  
+            const leadsRes = await axios.get(`https://graph.facebook.com/v19.0/${formId}/leads`, {
+                params: { access_token: pageToken },
+            });
+            console.log("leadsRes ---->",leadsRes)
+            setSuccess(leadsRes.data)
+        } catch (err) {
+            console.log(err)
+        }
+        fetch('https://graph.facebook.com/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({ query }),
+        }).then(res => res.json())
+            .then(data => {
+                setSuccess(data)
+                console.log(data)
+            })
+            .catch(err => {
+                setError(error)
+                console.error('Error:', err)
+            });
+
+    };
+
+
+
+
     return (
         <div>
             <h1 className="text-3xl font-bold underline">Social media signup example</h1>
